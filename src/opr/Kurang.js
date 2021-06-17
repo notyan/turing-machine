bc3b3b33ac7a35747f0027e08d6b7258b99af6d4
notyan <defaroyan@student.uns.ac.id>
2021-06-18 00:55:39 +0700

    Add Division

---
+++ src/opr/Kurang.js
@@ -0,0 +1,178 @@
+import React, { Component } from 'react'
+
+class Tambah extends Component {
+    state = {
+        iter: 1,
+        newList: [],
+        pos: -1,
+    }
+
+    listUpdate = (x,i) =>{
+        
+        let temp = this.state.newList.slice()
+        temp[i] = x
+        this.setState({
+            newList:temp
+        }, () => console.log("iterasi " + i +" diganti " + x))
+        
+    }
+    start(){
+        switch (this.state.pos) {
+            case -1:
+                this.init();
+                break;
+            case 0:
+                this.zero();
+                break;
+            case 1:
+                this.first();
+                break;
+            case 2:
+                this.second();
+                break;
+            case 3:
+                this.third();
+                break;
+            case 4:
+                this.forth();
+                break;
+            case 5:
+                this.fifth();
+                break;
+            case 6:
+                this.sixt();
+                break;
+            default:
+                break;
+        }
+    }
+    
+    stateUpdate = (i,pos) => {
+        this.setState({
+            iter: i,
+            pos: pos,
+        })
+    }
+    init(){
+        this.stateUpdate(this.state.iter, 0)
+    }
+    zero(){
+        if(this.state.newList[this.state.iter] === 0){
+            this.stateUpdate(this.state.iter+1, 0)
+        }else if(this.state.newList[this.state.iter] === 1){
+            this.stateUpdate(this.state.iter+1, 1)
+        }
+        
+        console.log("state " + this.state.pos)
+    }
+    first(){
+        if(this.state.newList[this.state.iter] === 0){
+            this.stateUpdate(this.state.iter+1, 1)
+        }else if(this.state.newList[this.state.iter] === 'B'){
+            this.stateUpdate(this.state.iter+1, 2)
+        }
+    }
+    second(){
+        if(this.state.newList[this.state.iter] === 0){
+            this.listUpdate('B', this.state.iter)
+            this.stateUpdate(this.state.iter-1, 3)
+        }else if(this.state.newList[this.state.iter] === 1 ){
+            this.listUpdate('B', this.state.iter)
+            this.stateUpdate(this.state.iter-1, 6)
+        }
+        console.log("iter " +  this.state.iter)
+        console.log("state " + this.state.pos)
+    }
+    third(){
+        if(this.state.newList[this.state.iter] === 0){
+            this.stateUpdate(this.state.iter-1, 3)
+        }else if(this.state.newList[this.state.iter] === 1){
+            this.stateUpdate(this.state.iter-1, 4)
+        }
+        console.log("iter " +  this.state.iter)
+        console.log("state " + this.state.pos)
+    }
+    forth(){
+        if(this.state.newList[this.state.iter] === 0){
+            this.stateUpdate(this.state.iter-1, 4)
+        }else if(this.state.newList[this.state.iter] === 'B' ){
+            let temp = this.state.newList
+            temp.push('B')
+            this.setState({newList:temp})
+            this.stateUpdate(this.state.iter+1, 0)
+        }
+        console.log("iter " + this.state.iter)
+        console.log("state " + this.state.pos)
+    }
+    fifth(){
+        if(this.state.newList[this.state.iter] === 0){
+            this.listUpdate('B', this.state.iter)
+        }else if(this.state.newList[this.state.iter] === 1){
+            this.stateUpdate(this.state.iter-1, 4)
+        }
+        console.log("iter " + this.state.iter)
+        console.log("state " + this.state.pos)
+    }
+    sixth(){
+        console.log("ENDED")
+        console.log("ENDED")
+    }
+    handleClick = () =>{
+        if(this.state.newList.length === 0){
+            this.setState({
+                newList: [...this.props.variable.list]
+            }, () => this.start(), console.log(this.state.newList) )
+        }else{
+            this.start()
+        }
+        
+    }
+    handleReset= () =>{
+        this.setState({
+            newList: [],
+            iter: 1,
+            pos: -1,  //-1 karena baru kepikiran di akhir kalau pas initiate tu belum dirubah
+        }, () => console.log(this.state.newList))
+    }
+
+
+    tapeMaker = (props) =>{
+        const variable = this.props.variable;
+        const print = this.state.newList.map((val, i) => {
+            if(i === this.state.iter){
+                return(
+                    <div className="box white">
+                        {val}
+                    </div>   
+                )
+            }else{
+                return (
+                    <div className="box">
+                        {val}
+                    </div>  
+                )
+            }
+
+            
+        })
+        return(
+            <div>
+                <input type="submit" id="" value="+" onClick={() => this.handleClick()}/>
+                <input type="submit" id="" value="reset" onClick={() => this.handleReset()}/>
+                <div className="scroll-container">
+                    {print}
+                </div>
+            </div>
+        )
+    }
+
+    render(){
+        return(
+            <div>
+                {this.tapeMaker()}
+            </div>
+        )
+    }
+}
+
+export default Tambah
