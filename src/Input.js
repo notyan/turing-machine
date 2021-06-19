2148d38f1608c56f1ef2fc040246c9476fb5d5af
notyan <defaroyan@student.uns.ac.id>
2021-06-19 23:13:53 +0700

    resize

--- src/Input.js
+++ src/Input.js
@@ -81,12 +81,16 @@ class Input extends Component{
         let opr = this.props.variable.delimiter
         console.log(opr)
         opr == null ?  satu = <span className="input-group-text no-right input-size" id="Input 1">Input 1</span> :
-            opr === 'exp' ? satu = <span className="input-group-text no-right lebar input-size" id="Input 1">Pangkat</span> : 
-                satu = <span className="input-group-text no-right input-size" id="Input 1">Input 1</span>
+            opr === 'exp' ? satu = <span className="input-group-text no-right lebar input-size" id="Input 1">Pangkat</span> :  
+                opr === '!' ? satu = <span className="input-group-text no-right lebar input-size" id="Input 1">Input</span> :
+                    opr === 'log' ? satu = <span className="input-group-text no-right lebar input-size" id="Input 1">Numerus</span> :
+                        satu = <span className="input-group-text no-right input-size" id="Input 1">Input 1</span>
         
         opr == null ?  dua = <span className="input-group-text no-right input-size" id="Input 1">Input 2</span> :
             opr === 'exp' ? dua = <span className="input-group-text no-right lebar input-size" id="Input 1">Basis</span> : 
-                dua = <span className="input-group-text no-right input-size" id="Input 1">Input 2</span>
+                opr === '!' ? dua = <span className="input-group-text no-right lebar input-size" id="Input 1">Isi dgn 0</span> :
+                    opr === 'log' ? dua = <span className="input-group-text no-right lebar input-size" id="Input 1">Basis</span> :
+                        dua = <span className="input-group-text no-right input-size" id="Input 1">Input 2</span>
         return(
             <div>
                 <h4>User Input</h4>
